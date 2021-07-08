46fafeb52b00ecb17e8004016a494cdde943edcc
Jim Ross <44807856+jimrossuk@users.noreply.github.com>
2021-07-08 16:07:07 +0100

    reinstalled wordpress
    agin

---
+++ app/public/wp-content/themes/twentytwentyone-child/page-parallax.php
@@ -0,0 +1,18 @@
+<?php
+get_header();
+
+?>
+    <img class="pic fade-in" src="/wp-content/uploads/2021/05/regulyVietnam1.png">
+<div class="parallax">  
+    <div class="parallax__layer parallax__layer--back ">
+        <div class="jim">This is the background</div>
+        <h2 class="ross">This is Bob Reguly</h2>
+        <h3>What is thie day for me in china</h3>
+    </div>
+    <div class="parallax__layer parallax__layer--base">
+        <div class="ross">This is the foreground</div>
+    </div>
+
+</div>
+
+</div>
