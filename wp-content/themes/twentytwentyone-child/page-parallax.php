f29f086f86cfbff6ca3ca1924b92c1c1a5f56255
Jim Ross <44807856+jimrossuk@users.noreply.github.com>
2021-07-08 15:20:05 +0100

    added a app js

--- wp-content/themes/twentytwentyone-child/page-parallax.php
+++ wp-content/themes/twentytwentyone-child/page-parallax.php
@@ -7,11 +7,7 @@ get_header();
     <div class="parallax__layer parallax__layer--back ">
         <div class="jim">This is the background</div>
         <h2 class="ross">This is Bob Reguly</h2>
-<<<<<<< HEAD
-        <h3>Wh1234 i want more</h3>
-=======
-        <h3>What is thie day for me in china</h3>
->>>>>>> parent of 06715d7... added more contnet
+        <h3>4 i want more</h3>
     </div>
     <div class="parallax__layer parallax__layer--base">
         <div class="ross">This is the foreground</div>
