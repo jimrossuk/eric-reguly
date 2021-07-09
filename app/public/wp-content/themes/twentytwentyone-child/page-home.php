9e838d329198b9e854d418d1f1f2b28b32b48da0
Jim Ross <44807856+jimrossuk@users.noreply.github.com>
2021-07-09 13:31:32 +0100

    added map to paralax background

--- app/public/wp-content/themes/twentytwentyone-child/page-home.php
+++ app/public/wp-content/themes/twentytwentyone-child/page-home.php
@@ -5,12 +5,12 @@ get_header();
     <img class="pic fade-in" src="/wp-content/uploads/2021/05/regulyVietnam1.png">
 <div class="parallax">  
     <div class="parallax__layer parallax__layer--back ">
-        <div class="jim">This is the background</div>
+        <div class="jim"> This is the background </div>
         <h2 class="ross">This is Bob Reguly</h2>
        
     </div>
     <div class="parallax__layer parallax__layer--base">
-        <div class="ross">This is the foreground</div>
+    <img src=" http://localhost:10013/wp-content/uploads/2021/07/empire-of-vietnam-vietnam-war-map-map.jpg  " >
     </div>
 
 </div>
