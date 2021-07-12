27b3231a1e6b6ad427ebbbbf0c6acf3250185e4a
Jim Ross <44807856+jimrossuk@users.noreply.github.com>
2021-07-12 12:17:33 +0100

    added js file.  changed fadeIn time on first image

--- app/public/wp-content/themes/twentytwentyone-child/functions.php
+++ app/public/wp-content/themes/twentytwentyone-child/functions.php
@@ -4,4 +4,5 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'theme-style', get_stylesheet_directory_uri() . '/custom-sass/style.css');
+    wp_enqueue_script( 'app-info', get_stylesheet_directory_uri() . '/app/js.js'); 
 }
