4db73476dc9b1aa834a26a9d17d0fae1d6e6a48d
Jim Ross <44807856+jimrossuk@users.noreply.github.com>
2021-08-19 15:54:47 +0100

    Title works for all @media lengths

--- app/public/wp-content/themes/twentytwentyone-child/functions.php
+++ app/public/wp-content/themes/twentytwentyone-child/functions.php
@@ -17,13 +17,4 @@ add_action('wp_enqueue_scripts', 'theme_scripts');
 wp_enqueue_script('jquery');
 
 
-// https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6/jquery.min.js
-
-
-// wp_enqueue_script( 'my-script', get_template_directory_uri() . '/app/js.js', array(), true );
-
-// <script
-// 			  src="https://code.jquery.com/jquery-3.6.0.min.js"
-// 			  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-// 			  crossorigin="anonymous"></script>
 		
